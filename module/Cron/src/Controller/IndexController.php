41403525e63c9a4a8d2f35a4744ccac686c0cbf5
Viktor <ivavictor@mail.ru>
2017-02-20 11:18:38 +0300

    Add zendframework/zend-queue

--- module/Cron/src/Controller/IndexController.php
+++ module/Cron/src/Controller/IndexController.php
@@ -9,6 +9,28 @@ class IndexController extends AbstractActionController
 
     public function indexAction()
     {
+        $options = array(
+            'name'             => 'def_queue',
+        );
+
+        $queue = new Queue('ArrayAdapter', $options);
+        $queue->send('first1');
+        $queue->send('first2');
+        $queue->send('first3');
+        var_dump($queue->count());
+        pr($queue);
+
+        $messages = $queue->receive(5);
+
+        foreach ($messages as $i => $message) {
+            echo $message->body, "\n";
+
+            // Сообщение обработано, его можно удалить
+            $queue->deleteMessage($message);
+        }
+        $queue->send('first4');
+        var_dump($queue->count());
+        pr($queue);
         echo 'Hello world';
         return $this->getResponse();
     }
