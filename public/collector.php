e39fe4eba37d9a19993b0979c062364ff62fe256
Viktor <ivavictor@mail.ru>
2018-05-28 21:23:37 +0300

    received data proxy

--- public/collector.php
+++ public/collector.php
@@ -4,10 +4,10 @@
 include __DIR__ . '/../vendor/autoload.php';
 
 
-define("MOEX_URL_CURRENCIES", 'http://iss.moex.com/iss/engines/currency/markets/selt/securities?securities=EUR_RUB__TOD,USD000000TOD');
+define("MOEX_URL_CURRENCIES", 'http://iss.moex.com/iss/engines/currency/markets/selt/securities.html?securities=EUR_RUB__TOD,USD000000TOD');
 
 
-file_put_contents('../data/cache/' . date('dmYHis') .'.html',file_get_contents(MOEX_URL_CURRENCIES));
+file_put_contents('../data/cache/' . date('dm H-i') .'.html',file_get_contents(MOEX_URL_CURRENCIES));
 
 function pr($val)
 {
